519cb0034ecfdaef330c0d1cbfc82b2a56ac4fbd
NikkitaTorres <nikkitatorres@yahoo.com>
2024-02-04 21:04:13 -0800

    routing WIP

--- VendorOrder/Program.cs
+++ VendorOrder/Program.cs
@@ -1,29 +1,49 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace VendorOrder
 {
-  class Program
-  {
-    static void Main(string[] args)
+    public class Program
     {
-      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-      builder.Services.AddControllersWithViews();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureServices((context, services) =>
+                    {
+                        services.AddControllersWithViews();
+                    });
 
-      WebApplication app = builder.Build();
+                    webBuilder.Configure((context, app) =>
+                    {
+                        if (context.HostingEnvironment.IsDevelopment())
+                        {
+                            app.UseDeveloperExceptionPage();
+                        }
+                        else
+                        {
+                            app.UseExceptionHandler("/Home/Error");
+                            app.UseHsts();
+                        }
 
-      
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
-      app.UseRouting();
+                        app.UseHttpsRedirection();
+                        app.UseStaticFiles();
+                        app.UseRouting();
 
-      app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-      );
-
-      app.Run();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllerRoute(
+                                name: "default",
+                                pattern: "{controller=Home}/{action=Index}/{id?}");
+                        });
+                    });
+                });
     }
-  }
 }
