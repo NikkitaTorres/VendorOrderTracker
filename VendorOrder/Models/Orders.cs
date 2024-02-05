aa0ec0fbb909e4579d7b81af10b42a6905824901
NikkitaTorres <nikkitatorres@yahoo.com>
2024-02-04 21:39:08 -0800

    working mvp

--- VendorOrder/Models/Orders.cs
+++ VendorOrder/Models/Orders.cs
@@ -1,14 +1,21 @@
-using System.Collections.Generic;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace VendorOrder.Models
 {
-  public class Order
-  {
-    public int OrderId { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public decimal Price { get; set; }
-    public DateTime Date { get; set; }
-  }
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a valid positive price")]
+        public decimal Price { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
 }
